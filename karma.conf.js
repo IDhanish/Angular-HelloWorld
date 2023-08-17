a3f5ec2527d3780bfc3ee818ba364fcfe9600398
Dhanish Ismail Haroon(UST <179893@ust-global.com>
2023-08-17 11:36:20 +0530

    modified karma conf file

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
 
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-edge-launcher'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
 
@@ -18,22 +18,10 @@ module.exports = function(config) {
     },
 
     preprocessors: {
-      'src/app/component.spec.ts': ['@angular-devkit/build-angular']
+      'src/app/app.component.spec.ts': ['@angular-devkit/build-angular']
     },
 
-    browsers: ['Chrome'],
-
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
-          '--remote-debugging-port=9222'
-        ]
-      }
-    },
+    browsers: ['Edge'],
 
     reporters: ['progress', 'kjhtml'],
 
